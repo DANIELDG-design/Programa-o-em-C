21cab4ba2a2075b648d3d58e3320ff6e4c1aa5df
DANIELDG-design <55002679+DANIELDG-design@users.noreply.github.com>
2019-11-08 20:04:04 -0300

    Add files via upload

---
+++ Antecessor e o seu sucessor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void);
+{
+	Float n; 
+	printf("Digite o numero:");
+	scanf("%f",&n );
+	printf("Antecessor:%f e Sucessor:%f",n-1,n+1);
+	system("pause");
+	return 0;
+}
+
---
+++ Imprimir sua idade.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdio.h>
+
+int main (void)
+{
+	char nome [100];
+	printf("Digite seu nome: \n");
+	scanf ("%s", &nome);
+	printf("\n%, nome");
+	
+	char idade[100];
+	printf("Digite sua Idade:\n");
+	scanf("%s", &idade);
+	printf("\n%,idade");
+	system("pause");
+	return 0;
+}
